71ba7321415139b0361cde0e3ede14b0e1fe46d6
Treeways <56928485+Treeways@users.noreply.github.com>
2024-01-18 20:08:29 -0600

    Added script failsafe so it's reusable

--- rename_project.sh
+++ rename_project.sh
@@ -5,9 +5,13 @@
 CURRENT_MOD_NAME="SDVModTemplate"
 
 # Enter your new project name here.
-MOD_NAME="YourProjectNameHere"
+MOD_NAME="SDVModTemplate"
 #####
 
+if [[ "$CURRENT_MOD_NAME" == "$MOD_NAME" ]] then
+        echo "Please edit this file and enter a new project name."
+        exit 1
+fi
 
 dotnet sln remove $CURRENT_MOD_NAME
 sed -i -e "s/${CURRENT_MOD_NAME}/${MOD_NAME}/g" ./${CURRENT_MOD_NAME}/ModEntry.cs
